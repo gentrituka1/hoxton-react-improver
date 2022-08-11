bbe2c32ba64ddaf327eabd04d764f920f5869923
Gentrit Uka <gentrit-uka@live.com>
2022-08-11 23:21:14 +0200

    updated

--- src/pages/TeamsDetails.tsx
+++ src/pages/TeamsDetails.tsx
@@ -25,19 +25,21 @@ export function TeamsDetails () {
     return (
         <>
             <div className="the-team">
-                <header className="team-details__header">{team.name} Football Club</header>
-                <main> 
-                    <div className="team-details__image">
-                        <img src={team.logo} alt={team.code} width={100} />
-                    </div>
-                    <div className="team-details__info">
-                        <div className="team-into-item"><span className="grey">FULL NAME:</span><span className="bold-grey">{team.name}</span></div>
-                        <div className="team-into-item"><span className="grey">CODE:</span><span className="bold-grey">{team.code}</span></div>
-                        <div className="team-into-item"><span className="grey">FOUNDED:</span><span className="bold-grey">{team.founded}</span></div>
-                        <div className="team-into-item"><span className="grey">GROUND:</span><span className="bold-grey">{team.stadium}</span></div>
-                        <div className="team-into-item"><span className="grey">GROUND CAPACITY:</span><span className="bold-grey">{team.stadiumCapacity}</span></div>
-                    </div>
-                </main>
+                <div className="the-team-details">
+                    <header className="team-details__header">{team.name} Football Club</header>
+                    <main> 
+                        <div className="team-details__image">
+                            <img src={team.logo} alt={team.code} width={100} />
+                        </div>
+                        <div className="team-details__info">
+                            <div className="team-info-item"><span className="grey">FULL NAME:</span><span className="bold-grey">{team.name}</span></div>
+                            <div className="team-info-item"><span className="grey">CODE:</span><span className="bold-grey">{team.code}</span></div>
+                            <div className="team-info-item"><span className="grey">FOUNDED:</span><span className="bold-grey">{team.founded}</span></div>
+                            <div className="team-info-item"><span className="grey">GROUND:</span><span className="bold-grey">{team.stadium}</span></div>
+                            <div className="team-info-item"><span className="grey">GROUND CAPACITY:</span><span className="bold-grey">{team.stadiumCapacity}</span></div>
+                        </div>
+                    </main>
+                 </div>
             </div>
         </>
     )
