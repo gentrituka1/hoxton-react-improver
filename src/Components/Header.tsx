7c3e7728091b640adb433120319a7c196d572663
Gentrit Uka <gentrit-uka@live.com>
2022-08-15 23:41:35 +0200

    updated

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -8,7 +8,19 @@ export function Header() {
             <img src="/premier-league.svg" alt="Premier League" width={200} />
             </Link>
             <div className="livescore-header-nav">
-                <Link className="link" to="/teams">Teams</Link>
+                <Link className="link" to="/teams" onClick={() => {
+                    return(
+                        <div className="livescore-header-nav__item">
+                            <ul>
+                                <li><h3>Premier League</h3></li>
+                                <li><h3>La Liga</h3></li>
+                                <li><h3>Bundesliga</h3></li>
+                                <li><h3>Ligue 1</h3></li>
+                                <li><h3>Serie A</h3></li>
+                            </ul>
+                        </div>
+                    )
+                }}>Teams</Link>
                 <Link className="link" to="/table">Table</Link>
                 <Link className="link" to="/favourites">Favourite Teams</Link>
             </div>
